f32e3941437db978016153e0c7f8094b387d53b5
Josiah Niedrauer <jniedrauer@gmail.com>
2017-05-21 15:47:32 -0500

    Add setup.py, __pkginfo__ and entrypoint script

---
+++ dfman/__pkginfo__.py
@@ -0,0 +1,35 @@
+# pylint: disable=invalid-name,redefined-builtin
+"""dfman package info"""
+
+
+import os
+
+
+modname = distname = 'dfman'
+numversion = (0, 0, 1)
+version = '.'.join([str(num) for num in numversion])
+
+install_requires = [
+]
+
+license = 'MIT'
+description = 'Dotfile Manager'
+url = 'https://github.com/jniedrauer/dfman'
+author = 'Josiah Niedrauer'
+author_email = 'jniedrauer@gmail.com'
+
+classifiers = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Environment :: Console',
+    'Intended Audience :: End Users/Desktop',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: POSIX',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3 :: Only',
+    'Topic :: Utilities'
+]
+
+
+long_description = """"""
+
+scripts = [os.path.join('bin', distname)]
